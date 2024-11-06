dd2dfdba4a0f797e83b40b202ba6b3da51c25298
bspt628 <hirotobspt628@gmail.com>
2024-11-06 19:33:45 +0900

    SQLの接続Goland上では成功

--- backend/main.go
+++ backend/main.go
@@ -3,25 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func main() {
+	//
+	//mysqlUser := os.Getenv("MYSQL_USER")
+	//mysqlPwd := os.Getenv("MYSQL_PWD")
+	//mysqlHost := os.Getenv("MYSQL_HOST")
+	//mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	http.ListenAndServe(":"+port, nil)
-
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlUser := "root"
+	mysqlPwd := "rootpass"
+	mysqlHost := "unix(/cloudsql/term6-hiroto-uchida:us-central1:uttc6)"
+	mysqlDatabase := "hackathon"
 
 	// データベースを初期化します
 	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
