cee839f9b7ae5b1c06872c583d97362f8ddab0d1
bspt628 <hirotobspt628@gmail.com>
2024-12-04 04:08:16 +0900

    Dockerfileの変更

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'hackathon',
-  location: 'asia-east1'
-};
-exports.connectorConfig = connectorConfig;
-
