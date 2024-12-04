d7700689dcf4248ee455bc7f8e706e4ec0ff1c26
bspt628 <hirotobspt628@gmail.com>
2024-12-04 18:07:11 +0900

    follow重複えらーの設計

--- backend/internal/dao/follow/follow_add.go
+++ backend/internal/dao/follow/follow_add.go
@@ -4,13 +4,24 @@ import (
 	"context"
 	"database/sql"
 	"hackathon/db/sqlc/generated"
+	"github.com/go-sql-driver/mysql"
+	"errors"
 )
 
 func (dao *FollowDAO) AddFollow(ctx context.Context, followID, followerID, followingID string) error {
 	// フォロー情報を保存
-	return dao.queries.AddFollow(ctx, sqlc.AddFollowParams{
+	err := dao.queries.AddFollow(ctx, sqlc.AddFollowParams{
 		ID: followID, 
 		FollowerID: sql.NullString{String: followerID, Valid: true},
 		FollowingID: sql.NullString{String: followingID, Valid: true},
 	})
+
+	if errmysql, ok := err.(*mysql.MySQLError); ok {
+		if errmysql.Number == 1062 {
+			return errors.New("follow already exists")
+		}
+	}else if err != nil {
+		return errors.New("failed to add follow")
+	}
+	return nil
 }
