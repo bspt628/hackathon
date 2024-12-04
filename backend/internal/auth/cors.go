8361b6d3838c6e29239a317e9766101a02e07bca
bspt628 <hirotobspt628@gmail.com>
2024-12-04 21:39:47 +0900

    CORSミドルウェアの変更

--- backend/internal/auth/cors.go
+++ backend/internal/auth/cors.go
@@ -11,14 +11,16 @@ func CORS(next http.Handler) http.Handler {
 		fmt.Println("CORS middleware in use")
 		// 必要な CORS ヘッダーを設定
 		w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-five-rho.vercel.app") // "*" は任意のオリジンを許可
+		w.Header().Set("Access-Control-Allow-Credentials", "true") // クッキーを許可
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS") // 許可する HTTP メソッド
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // 許可するヘッダー
 
 		// プリフライトリクエスト (OPTIONS) に対して早期に応答
 		if r.Method == "OPTIONS" {
-			return
+			w.WriteHeader(http.StatusOK)
 		}
 		// ヘッダーをコンソールに出力
+
 		fmt.Println("Request Headers: ", r.Header)
 
 		next.ServeHTTP(w, r)
