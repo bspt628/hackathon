39e1ae20be262db1c1bbb9969d326eb78bce6bea
bspt628 <hirotobspt628@gmail.com>
2024-12-05 18:29:25 +0900

    repost createのincrementの実装

--- backend/internal/dao/repost/repost.go
+++ backend/internal/dao/repost/repost.go
@@ -3,6 +3,7 @@ package repostdao
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"hackathon/db/sqlc/generated"
 	"hackathon/internal/model"
 )
@@ -15,6 +16,32 @@ func (dao *RepostDAO) CreateRepost(ctx context.Context, arg model.CreateRepostPa
 		IsQuoteRepost: sql.NullBool{Bool: arg.IsQuoteRepost, Valid: true},
 		AdditionalComment: sql.NullString{String: arg.AdditionalComment, Valid: true},
 	}
+
+	// トランザクションを開始
+	tx, err := dao.db.BeginTx(ctx, nil)
+	if err != nil {
+		return errors.New("failed to begin transaction")
+	}
+	defer tx.Rollback()
+
+	// リポストを登録
+	err = dao.queries.CreateRepost(ctx, argdao)
+	if err != nil {
+		return errors.New("failed to create repost")
+	}
+
+	// リポスト数をインクリメント
+	_, err = dao.queries.IncrementRepostsCount(ctx, arg.OriginalPostID)
+	if err != nil {
+		return errors.New("failed to increment repost count")
+	}
+
+	// トランザクションをコミット
+	err = tx.Commit()
+	if err != nil {
+		return errors.New("failed to commit transaction")
+	}
+
 
 	return dao.queries.CreateRepost(ctx, argdao)
 }
--- backend/internal/usecase/repost/repost.go
+++ backend/internal/usecase/repost/repost.go
@@ -6,7 +6,6 @@ import (
 )
 
 func (rc *RepostUsecase) CreateRepost(ctx context.Context, params model.CreateRepostParams) error {
-	// ulidを生成
 	
 	// リポスト情報をDAOを通じて保存
 	return rc.dao.CreateRepost(ctx, params)
