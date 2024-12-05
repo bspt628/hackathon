f6a6f6ff35aaa5eb6fe663210a9b031c07885868
bspt628 <hirotobspt628@gmail.com>
2024-12-05 12:06:17 +0900

    followにインクリメントデクリメントを実装

--- backend/internal/dao/follow/follow_add.go
+++ backend/internal/dao/follow/follow_add.go
@@ -9,19 +9,44 @@ import (
 )
 
 func (dao *FollowDAO) AddFollow(ctx context.Context, followID, followerID, followingID string) error {
+	// トランザクションを開始
+	tx, err := dao.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
 	// フォロー情報を保存
-	err := dao.queries.AddFollow(ctx, sqlc.AddFollowParams{
+	err = dao.queries.AddFollow(ctx, sqlc.AddFollowParams{
 		ID: followID, 
 		FollowerID: sql.NullString{String: followerID, Valid: true},
 		FollowingID: sql.NullString{String: followingID, Valid: true},
 	})
-
-	if errmysql, ok := err.(*mysql.MySQLError); ok {
-		if errmysql.Number == 1062 {
-			return errors.New("follow already exists")
+	if err != nil {
+		if errmysql, ok := err.(*mysql.MySQLError); ok {
+			if errmysql.Number == 1062 {
+				return errors.New("follow already exists")
+			}
+		}else {
+			return errors.New("failed to add follow")
 		}
-	}else if err != nil {
-		return errors.New("failed to add follow")
 	}
+	// フォロワー数をインクリメント
+	_, err = dao.queries.IncrementFollowersCount(ctx, followingID)
+	if err != nil {
+		return errors.New("failed to IncrementFollowersCount")
+	}
+	// フォロー数をインクリメント		
+	_, err = dao.queries.IncrementFollowingsCount(ctx, followerID)
+	if err != nil {
+		return errors.New("failed to IncrementFollowingsCount")
+	}
+
+	// トランザクションをコミット
+	err = tx.Commit()
+	if err != nil {
+		return errors.New("failed to commit transaction")
+	}
+	
 	return nil
 }
--- backend/internal/dao/follow/remove.go
+++ backend/internal/dao/follow/remove.go
@@ -8,6 +8,13 @@ import (
 )
 
 func (dao *FollowDAO) RemoveFollow(ctx context.Context, followerID, followingID string) error {
+	// トランザクションを開始
+	tx, err := dao.db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer tx.Rollback()
+
 	// フォロー情報を保存
 	result, err := dao.queries.RemoveFollow(ctx, sqlc.RemoveFollowParams{
 		FollowerID: sql.NullString{String: followerID, Valid: true},
@@ -25,5 +32,24 @@ func (dao *FollowDAO) RemoveFollow(ctx context.Context, followerID, followingID 
 	if rowsAffected == 0 {
 		return fmt.Errorf("no follow found from id %s to id %s", followerID, followingID)
 	}
+
+	// フォロワー数をデクリメント
+	_, err = dao.queries.DecrementFollowersCount(ctx, followingID)
+	if err != nil {
+		return fmt.Errorf("failed to decrement followers count: %w", err)
+	}
+
+	// フォロー数をデクリメント
+	_, err = dao.queries.DecrementFollowingsCount(ctx, followerID)
+	if err != nil {
+		return fmt.Errorf("failed to decrement followings count: %w", err)
+	}
+
+	// トランザクションをコミット
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
 	return err
 }
