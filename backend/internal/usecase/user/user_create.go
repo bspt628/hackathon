10356f55229888d3341408fef252db27a9aa888d
bspt628 <hirotobspt628@gmail.com>
2024-12-06 00:47:30 +0900

    create user の usernameから特殊文字を弾く

--- backend/internal/usecase/user/user_create.go
+++ backend/internal/usecase/user/user_create.go
@@ -7,12 +7,19 @@ import (
 	"github.com/oklog/ulid"
 	"math/rand"
 	"time"
+	"strings"
+	"errors"
 )
 
 func (uc *UserUsecase) CreateUser(ctx context.Context, email, password, username, displayname string) (*sqlc.User, error) {
 	// IDをulidで自動生成する
 	entropy := rand.New(rand.NewSource(time.Now().UnixNano()))
 	myid := ulid.MustNew(ulid.Now(), entropy).String()
+
+	// usernameに特殊文字が含まれているかチェック
+	if !uc.CheckUsername(username) {
+		return nil, errors.New("usernameに特殊文字が含まれています")
+	}
 
 	// bcyptでパスワードをハッシュ化
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -29,4 +36,10 @@ func (uc *UserUsecase) CreateUser(ctx context.Context, email, password, username
 
 	// 作成されたユーザー情報を返す
 	return user, nil
+}
+
+// CheckUsername はusernameに特殊文字が含まれているかチェックする
+func (uc *UserUsecase) CheckUsername(username string) bool {
+	// usernameに特殊文字が含まれているかチェック
+	return  !strings.ContainsAny(username, "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~") 
 }
