bb08233c7ee62d81fc6b8931ff55393f0f8b1c94
bspt628 <hirotobspt628@gmail.com>
2024-12-05 14:49:32 +0900

    followingの log作成

--- backend/internal/usecase/follow/followingscount.go
+++ backend/internal/usecase/follow/followingscount.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 )
 
 func (fc *FollowUsecase) GetFollowingsCount(ctx context.Context, userID string) (int32, error) {
 	if fc == nil || fc.followDAO == nil {
 		return 0, errors.New("UserUseCase or UserDAO is nil")
 	}
+	log.Println("GetFollowingsCount: ", userID)
 
 	// フォロー情報を保存
 	count, err := fc.followDAO.GetFollowingsCount(ctx, userID)
