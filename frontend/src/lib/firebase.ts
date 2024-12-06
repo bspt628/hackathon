bc3929f4059ca2eea1d1794eab8684d54a4de077
bspt628 <hirotobspt628@gmail.com>
2024-12-06 13:34:28 +0900

    .envの完成

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCI6Sny25W-XtA8xIp1YV13epZkUxZGe4s",
-  authDomain: "term6-hiroto-uchida.firebaseapp.com",
-  databaseURL: "https://term6-hiroto-uchida-default-rtdb.firebaseio.com",
-  projectId: "term6-hiroto-uchida",
-  storageBucket: "term6-hiroto-uchida.firebasestorage.app",
-  messagingSenderId: "241499864821",
-  appId: "1:241499864821:web:c39f3db60824b5b5df8fbc"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized already
